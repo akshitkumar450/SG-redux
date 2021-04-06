29a3b0a9d2d14c76e42031662d70da6f08bbc699
Akshit Kumar <akshitkumar450@gmail.com>
2021-04-06 18:28:44 +0530

    creating store and Provider method

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from 'redux'
+
 export const songsReducer = () => {
     return [
         {
@@ -26,3 +28,8 @@ export const selectedSongReducer = (selectedSong = null, action) => {
 
     return selectedSong
 }
+
+export default combineReducers({
+    songs: songsReducer,
+    selectedSong: selectedSongReducer
+})
