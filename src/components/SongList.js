a682389f4b8962e769b4ba03095e9982064e381f
Akshit Kumar <akshitkumar450@gmail.com>
2021-04-06 20:19:08 +0530

    updating the state by action creater

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { selectSong } from '../Actions'
 class SongList extends Component {
     renderList = () => {
         return this.props.songs.map((song) => {
             return (
                 <div className='item' key={song.title}>
                     <div className='right floated content'>
-                        <button className='ui button primary'>select</button>
+                        <button
+                            // when the btn clicks we are updating the state (redux store) of selectedSong in reducer
+                            onClick={() => this.props.selectSong(song)}
+                            className='ui button primary'>
+
+                            select</button>
                     </div>
                     <div className='content'>
                         {song.title}
@@ -28,9 +34,10 @@ class SongList extends Component {
 // state is the object which is data stored in the redux store
 // and the data  can be passed to components as props 
 const mapStateToProps = (state) => {
+    // console.log(state);
     return {
         songs: state.songs
     }
 }
-
-export default connect(mapStateToProps)(SongList)
+// selectSong action creater will be available in component as a props
+export default connect(mapStateToProps, { selectSong: selectSong })(SongList)
